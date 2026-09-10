ee578338252453ceb9f48a6555592bbdf1812e1c
안율 <an-yul@an-yul-ui-MacBookPro.local>
2026-09-10 13:11:58 +0900

    Initial commit

---
+++ GOMS-Mission01/GOMS_Mission01App.swift
@@ -0,0 +1,17 @@
+//
+//  GOMS_Mission01App.swift
+//  GOMS-Mission01
+//
+//  Created by 안율 on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct GOMS_Mission01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
